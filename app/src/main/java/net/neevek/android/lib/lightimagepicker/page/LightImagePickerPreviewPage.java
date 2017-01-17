35646bef04cdc35f68e991762c77ab399abf2f8e
neevek <i@neevek.net>
2017-01-17 19:34:09 +0800

    tested on APILevel-16, no need to restore SystemUiVisibility

--- app/src/main/java/net/neevek/android/lib/lightimagepicker/page/LightImagePickerPreviewPage.java
+++ app/src/main/java/net/neevek/android/lib/lightimagepicker/page/LightImagePickerPreviewPage.java
@@ -67,7 +67,6 @@ public class LightImagePickerPreviewPage extends Page
 
     private int mMaxAllowedSelection = 9;
     private int mStartItemIndex;
-    private int mOrigSystemUiVisibility;
 
     public static LightImagePickerPreviewPage create(PageActivity pageActivity, int maxAllowedSelection) {
         LightImagePickerPreviewPage previewPage = new LightImagePickerPreviewPage(pageActivity);
@@ -106,8 +105,9 @@ public class LightImagePickerPreviewPage extends Page
         super.onShow();
 
         if (Build.VERSION.SDK_INT >= 16) {
-            mOrigSystemUiVisibility = getContext().getWindow().getDecorView().getSystemUiVisibility();
-            getContext().getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+            getContext().getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         }
 
         if (mResourceList == null) {
@@ -137,9 +137,6 @@ public class LightImagePickerPreviewPage extends Page
     }
 
     private void clearFullScreenFlags() {
-        if (Build.VERSION.SDK_INT >= 16) {
-            getContext().getWindow().getDecorView().setSystemUiVisibility(mOrigSystemUiVisibility);
-        }
         getContext().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 
